6b2ac453e54b7a93dc406556bc2bdc6221be3147
cgeek <cem.moreau@gmail.com>
2016-11-28 12:02:21 +0100

    Fixing duniter/duniter#736 Not building on AppVeyor

--- misc/duniterConfigToCesium.js
+++ misc/duniterConfigToCesium.js
@@ -2,16 +2,23 @@ var fs = require('fs');
 function getUserHome() {
     return process.env.HOME || process.env.USERPROFILE;
 }
-var duniterConf = JSON.parse(fs.readFileSync(getUserHome()+'/.config/duniter/duniter_default/conf.json', 'utf8'));
-var host = duniterConf.remotehost;
-if(!host) host = duniterConf.remoteipv4;
-if(!host) host = duniterConf.remoteipv6;
-var cesiumConf = {
-    "default": {
-        "node":{
-            "host":host,
-            "port":duniterConf.remoteport
+
+try {
+
+    var duniterConf = JSON.parse(fs.readFileSync(getUserHome()+'/.config/duniter/duniter_default/conf.json', 'utf8'));
+    var host = duniterConf.remotehost;
+    if(!host) host = duniterConf.remoteipv4;
+    if(!host) host = duniterConf.remoteipv6;
+    var cesiumConf = {
+        "default": {
+            "node":{
+                "host":host,
+                "port":duniterConf.remoteport
+            }
         }
-    }
-};
-fs.writeFileSync('cesium/app/config.json',JSON.stringify(cesiumConf));
+    };
+    fs.writeFileSync('cesium/app/config.json',JSON.stringify(cesiumConf));
+
+} catch (e) {
+    console.log('Not Duniter configuration found, skipping Cesium configuration.');
+}
