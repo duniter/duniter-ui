65acef7051e43b1c53e411717df57d6a6fde2822
Baptiste Lemoine <contact@cipherbliss.com>
2020-04-05 16:28:56 +0200

    :zap: add translation file in French

--- app/js/lib/conf/translate.js
+++ app/js/lib/conf/translate.js
@@ -3,6 +3,7 @@ module.exports = (app) => {
   app.config(['$translateProvider', ($translateProvider) => {
 
     $translateProvider.translations('en', require('./i18n/en'));
+    $translateProvider.translations('fr', require('./i18n/fr'));
 
     // Default language
     $translateProvider.preferredLanguage('en');
